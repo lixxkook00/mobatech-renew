4f0e9dd5bfebc6fa5edb68cdf69c2851f9671119
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-10-04 11:40:54 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,22 @@ $('#tokenomic-main-charac').onmouseleave = function(e) {
 // loading animation
 window.addEventListener('load', (event) => {
   $('.loading').style.display = "none"
+
+  if(window.screen.width>739){
+    let glowInTexts = document.querySelectorAll(".glowIn");
+  
+    glowInTexts.forEach(glowInText => {
+      let letters = glowInText.textContent.split("");
+      glowInText.textContent = "";
+      letters.forEach((letter, i) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.style.animationDelay = `${i * 0.02}s`;
+        glowInText.append(span);
+      });
+    });
+  }
+
 });
 
 // * Fullpage js init */
